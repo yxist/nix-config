f58abb01948408dfe0a2a328d76d99e1d77d775b
Peter Mann <pm@xdd.sk>
2020-08-24 22:31:12 +0200

    faweuioawfhioawf howfaho iafwhoiafw hoi awfhio

--- nix/i3.nix
+++ nix/i3.nix
@@ -9,16 +9,16 @@ in
     terminus_font
     terminus_font_ttf
     tamsyn
-    # TODO termsyn
+
+    ${pkgs.fetchFromGitHub {
+      owner = "Tecate";
+      repo = "bitmap-fonts";
+      rev = "5c101c91bf2ed0039aad02f9bf76ddb2018b1f21";
+      sha256 = "0s119zln3yrhhscfwkjncj72cr68694643009aam63s2ng4hsmfl";
+    }}
   ];
 
-  fonts.fonts = [ "${pkgs.fetchFromGitHub {
-    owner = "Tecate";
-    repo = "bitmap-fonts";
-    rev = "5c101c91bf2ed0039aad02f9bf76ddb2018b1f21";
-    sha256 = "0s119zln3yrhhscfwkjncj72cr68694643009aam63s2ng4hsmfl";
-  }}/bitmap" ];
-  fonts.fontconfig.allowBitmaps = true;
+  fonts.fontconfig.enable = true;
 
   xsession.enable = true;
   xsession.scriptPath = ".hm-xsession";
