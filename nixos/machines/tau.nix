4313d4a82b5ddcd255705055eb73f67abb6da911
Peter Mann <pm@xdd.sk>
2020-08-31 07:42:34 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAaaWuAdaA

--- nixos/machines/tau.nix
+++ nixos/machines/tau.nix
@@ -13,7 +13,7 @@
   networking.hostName = "tau";
 
   networking.useDHCP = false;
-  networking.enableIPv5 = true;
+  networking.enableIPv6 = true;
   networking.interfaces.ens3.useDHCP = true;
   networking.interfaces.ens3.ipv6.addresses = [{address = "2a01:4f8:c0c:4109::"; prefixLength = 64;}];
   networking.interfaces.ens3.ipv6.routes = [{address = "fe80::1";}];
