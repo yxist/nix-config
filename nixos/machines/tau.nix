473096c0e3ce62fb2b820f18a4362d7f364ceb4d
Peter Mann <pm@xdd.sk>
2020-09-15 02:40:06 +0200

    hihi

--- nixos/machines/tau.nix
+++ nixos/machines/tau.nix
@@ -87,7 +87,7 @@
       local all all              ident  map=local
       host  all all 127.0.0.1/32 md5
       host  all all ::1/128      md5
-    ''
+    '';
     identMap =''
       local git gitea
     '';
