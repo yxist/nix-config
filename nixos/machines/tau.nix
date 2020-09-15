bc1a0d91b9681a9d5eba01d487b4c4513085a351
Peter Mann <pm@xdd.sk>
2020-09-15 02:11:53 +0200

    grhiopae

--- nixos/machines/tau.nix
+++ nixos/machines/tau.nix
@@ -60,7 +60,7 @@
     httpAddress = "127.0.0.1";
     httpPort = 3001;
     database = {
-      socket = "/run/postgresql/"
+      socket = "/run/postgresql/";
       createDatabase = false;
       type = "postgres";
     };
